b9d99298bb66a3ad1d084ab0bfe18558f94d84d3
ecxm02 <familychan1@gmail.com>
2025-04-12 00:32:18 +0800

    Add XIntercept (Refactor of Line Intercept) (Yet to be tested)

--- FPGraph.srcs/sources_1/new/XAxisIntercepts.v
+++ FPGraph.srcs/sources_1/new/XAxisIntercepts.v
@@ -8,185 +8,310 @@ module XAxisIntercepts(
 
     //-----------------Graph Equation Inputs-------------------
     input                a1Sign,
-    input       [23:0]   a1Integer, a1Decimal,
+    input       [13:0]   a1Integer,
     input                b1Sign,
-    input       [23:0]   b1Integer, b1Decimal,
+    input       [13:0]   b1Integer,
     input                c1Sign,
-    input       [23:0]   c1Integer, c1Decimal,
-
+    input       [13:0]   c1Integer,
+    
     input                a2Sign,
-    input       [23:0]   a2Integer, a2Decimal,
+    input       [13:0]   a2Integer,
     input                b2Sign,
-    input       [23:0]   b2Integer, b2Decimal,
+    input       [13:0]   b2Integer,
     input                c2Sign,
-    input       [23:0]   c2Integer, c2Decimal,
+    input       [13:0]   c2Integer,
 
     input                a3Sign,
-    input       [23:0]   a3Integer, a3Decimal,
+    input       [13:0]   a3Integer,
     input                b3Sign,
-    input       [23:0]   b3Integer, b3Decimal,
+    input       [13:0]   b3Integer,
     input                c3Sign,
-    input       [23:0]   c3Integer, c3Decimal,
+    input       [13:0]   c3Integer,
     //---------------------------------------------------------
 
-    //---------------Graph 1 X-Intercepts----------------------
-    output           g1_X1Sign,
-    output  [23:0]   g1_X1Int, g1_X1Dec,
-
-    output           g1_Y1Sign,
-    output  [23:0]   g1_Y1Int, g1_Y1Dec,
-
-    output           g1_X2Sign,
-    output  [23:0]   g1_X2Int, g1_X2Dec,
-
-    output           g1_Y2Sign,
-    output  [23:0]   g1_Y2Int, g1_Y2Dec,
-
-    output           g1_isCalculated,
+    //---------------Graph 1 Intercepts----------------
+    output reg           g1_X1Sign,
+    output reg [13:0]   g1_X1Int, g1_X1Dec,
+    
+    output reg           g1_Y1Sign,
+    output reg [13:0]   g1_Y1Int, g1_Y1Dec,
+    
+    output reg           g1_X2Sign,
+    output reg [13:0]   g1_X2Int, g1_X2Dec,
+    
+    output reg           g1_Y2Sign,
+    output reg [13:0]   g1_Y2Int, g1_Y2Dec,
+    
+    output reg [1:0]    g1_isCalculated,
     //---------------------------------------------------------
 
-    //---------------Graph 2 X-Intercepts----------------------
-    output           g2_X1Sign,
-    output  [23:0]   g2_X1Int, g2_X1Dec,
-
-    output           g2_Y1Sign,
-    output  [23:0]   g2_Y1Int, g2_Y1Dec,
-
-    output           g2_X2Sign,
-    output  [23:0]   g2_X2Int, g2_X2Dec,
-
-    output           g2_Y2Sign,
-    output  [23:0]   g2_Y2Int, g2_Y2Dec,
-
-    output           g2_isCalculated,
+    //---------------Graph 2 Intercepts----------------
+    output reg           g2_X1Sign,
+    output reg [13:0]   g2_X1Int, g2_X1Dec,
+    
+    output reg           g2_Y1Sign,
+    output reg [13:0]   g2_Y1Int, g2_Y1Dec,
+    
+    output reg           g2_X2Sign,
+    output reg [13:0]   g2_X2Int, g2_X2Dec,
+    
+    output reg           g2_Y2Sign,
+    output reg [13:0]   g2_Y2Int, g2_Y2Dec,
+    
+    output reg [1:0]    g2_isCalculated,
     //----------------------------------------------------------
 
-    //---------------Graph 3 X-Intercepts-----------------------
-    output           g3_X1Sign,
-    output  [23:0]   g3_X1Int, g3_X1Dec,
-
-    output           g3_Y1Sign,
-    output  [23:0]   g3_Y1Int, g3_Y1Dec,
-
-    output           g3_X2Sign,
-    output  [23:0]   g3_X2Int, g3_X2Dec,
-
-    output           g3_Y2Sign,
-    output  [23:0]   g3_Y2Int, g3_Y2Dec,
-
-    output           g3_isCalculated
+    //---------------Graph 3 Intercepts-----------------
+    output reg           g3_X1Sign,
+    output reg [13:0]   g3_X1Int, g3_X1Dec,
+    
+    output reg           g3_Y1Sign,
+    output reg [13:0]   g3_Y1Int, g3_Y1Dec,
+    
+    output reg           g3_X2Sign,
+    output reg [13:0]   g3_X2Int, g3_X2Dec,
+    
+    output reg           g3_Y2Sign,
+    output reg [13:0]   g3_Y2Int, g3_Y2Dec,
+    
+    output reg [1:0]    g3_isCalculated
     //----------------------------------------------------------
 );
 
-    // Instantiate IntersectionQuadratic for Graph 1 X-Intercepts
-    IntersectionQuadratic g1XIntercept_inst (
+    localparam NOT_CALCULATED = 0, CALCULATED = 1, NO_SOLUTION = 2;
+    localparam ADD = 0, SUB = 1, MUL = 2, DIV = 3;
+    localparam POS = 0, NEG = 1;
+
+    // Instantiate IntersectionQuadratic
+
+    // Inputs to IntersectionQuadratic for equation coefficients
+    reg        temp_a1Sign;
+    reg [13:0] temp_a1Integer;
+    reg        temp_b1Sign;
+    reg [13:0] temp_b1Integer;
+    reg        temp_c1Sign;
+    reg [13:0] temp_c1Integer;
+
+    reg        temp_a2Sign;
+    reg [13:0] temp_a2Integer;
+    reg        temp_b2Sign;
+    reg [13:0] temp_b2Integer;
+    reg        temp_c2Sign;
+    reg [13:0] temp_c2Integer;
+
+    // Outputs from IntersectionQuadratic
+    wire       temp_X1Sign;
+    wire [13:0] temp_X1Int;
+    wire [13:0] temp_X1Dec;
+
+    wire       temp_Y1Sign;
+    wire [13:0] temp_Y1Int;
+    wire [13:0] temp_Y1Dec;
+
+    wire       temp_X2Sign;
+    wire [13:0] temp_X2Int;
+    wire [13:0] temp_X2Dec;
+
+    wire       temp_Y2Sign;
+    wire [13:0] temp_Y2Int;
+    wire [13:0] temp_Y2Dec;
+
+    reg startIntersectionQuadratic = 0;
+    wire [1:0]      temp_isCalculated;
+    
+    IntersectionQuadratic IntersectionQuadratic_inst (
         .basysClock       (basysClock),
-        .startCalculate   (startCalculate),
-        .a1Sign           (a1Sign),
-        .a1Integer        (a1Integer),
-        .a1Decimal        (a1Decimal),
-        .b1Sign           (b1Sign),
-        .b1Integer        (b1Integer),
-        .b1Decimal        (b1Decimal),
-        .c1Sign           (c1Sign),
-        .c1Integer        (c1Integer),
-        .c1Decimal        (c1Decimal),
-        .a2Sign           (0),
-        .a2Integer        (0),
-        .a2Decimal        (0),
-        .b2Sign           (0),
-        .b2Integer        (0),
-        .b2Decimal        (0),
-        .c2Sign           (0),
-        .c2Integer        (0),
-        .c2Decimal        (0),
-        .x1Sign           (g1_X1Sign),
-        .x1Integer        (g1_X1Int),
-        .x1Decimal        (g1_X1Dec),
-        .y1Sign           (g1_Y1Sign),
-        .y1Integer        (g1_Y1Int),
-        .y1Decimal        (g1_Y1Dec),
-        .x2Sign           (g1_X2Sign),
-        .x2Integer        (g1_X2Int),
-        .x2Decimal        (g1_X2Dec),
-        .y2Sign           (g1_Y2Sign),
-        .y2Integer        (g1_Y2Int),
-        .y2Decimal        (g1_Y2Dec),
-        .isCalculated     (g1_isCalculated)
+        .startCalculate   (startIntersectionQuadratic),
+        .a1Sign           (temp_a1Sign),
+        .a1Integer        (temp_a1Integer),
+        .b1Sign           (temp_b1Sign),
+        .b1Integer        (temp_b1Integer),
+        .c1Sign           (temp_c1Sign),
+        .c1Integer        (temp_c1Integer),
+        .a2Sign           (temp_a2Sign),
+        .a2Integer        (temp_a2Integer),
+        .b2Sign           (temp_b2Sign),
+        .b2Integer        (temp_b2Integer),
+        .c2Sign           (temp_c2Sign),
+        .c2Integer        (temp_c2Integer),
+        .x1Sign           (temp_X1Sign),
+        .x1Integer        (temp_X1Int),
+        .x1Decimal        (temp_X1Dec),
+        .y1Sign           (temp_Y1Sign),
+        .y1Integer        (temp_Y1Int),
+        .y1Decimal        (temp_Y1Dec),
+        .x2Sign           (temp_X2Sign),
+        .x2Integer        (temp_X2Int),
+        .x2Decimal        (temp_X2Dec),
+        .y2Sign           (temp_Y2Sign),
+        .y2Integer        (temp_Y2Int),
+        .y2Decimal        (temp_Y2Dec),
+        .isCalculated     (temp_isCalculated)
     );
 
-    // Instantiate IntersectionQuadratic for Graph 2 X-Intercepts
-    IntersectionQuadratic g2XIntercept_inst (
-        .basysClock       (basysClock),
-        .startCalculate   (startCalculate),
-        .a1Sign           (a1Sign),
-        .a1Integer        (a1Integer),
-        .a1Decimal        (a1Decimal),
-        .b1Sign           (b1Sign),
-        .b1Integer        (b1Integer),
-        .b1Decimal        (b1Decimal),
-        .c1Sign           (c1Sign),
-        .c1Integer        (c1Integer),
-        .c1Decimal        (c1Decimal),
-        .a2Sign           (0),
-        .a2Integer        (0),
-        .a2Decimal        (0),
-        .b2Sign           (0),
-        .b2Integer        (0),
-        .b2Decimal        (0),
-        .c2Sign           (0),
-        .c2Integer        (0),
-        .c2Decimal        (0),
-        .x1Sign           (g2_X1Sign),
-        .x1Integer        (g2_X1Int),
-        .x1Decimal        (g2_X1Dec),
-        .y1Sign           (g2_Y1Sign),
-        .y1Integer        (g2_Y1Int),
-        .y1Decimal        (g2_Y1Dec),
-        .x2Sign           (g2_X2Sign),
-        .x2Integer        (g2_X2Int),
-        .x2Decimal        (g2_X2Dec),
-        .y2Sign           (g2_Y2Sign),
-        .y2Integer        (g2_Y2Int),
-        .y2Decimal        (g2_Y2Dec),
-        .isCalculated     (g2_isCalculated)
+
+    wire clk100Khz;
+
+    // Instantiate the flexible_clock module with CLK_DIV = 50000 to get 1kHz output:
+    flexible_clock #(.CLK_DIV(500)) clk_gen (
+        .clk_in(basysClock),
+        .clk_out(clk100Khz)
     );
 
-    // Instantiate IntersectionQuadratic for Graph 3 X-Intercepts
-    IntersectionQuadratic g3XIntercept_inst (
-        .basysClock       (basysClock),
-        .startCalculate   (startCalculate),
-        .a1Sign           (a2Sign),
-        .a1Integer        (a2Integer),
-        .a1Decimal        (a2Decimal),
-        .b1Sign           (b2Sign),
-        .b1Integer        (b2Integer),
-        .b1Decimal        (b2Decimal),
-        .c1Sign           (c2Sign),
-        .c1Integer        (c2Integer),
-        .c1Decimal        (c2Decimal),
-        .a2Sign           (0),
-        .a2Integer        (0),
-        .a2Decimal        (0),
-        .b2Sign           (0),
-        .b2Integer        (0),
-        .b2Decimal        (0),
-        .c2Sign           (0),
-        .c2Integer        (0),
-        .c2Decimal        (0),
-        .x1Sign           (g3_X1Sign),
-        .x1Integer        (g3_X1Int),
-        .x1Decimal        (g3_X1Dec),
-        .y1Sign           (g3_Y1Sign),
-        .y1Integer        (g3_Y1Int),
-        .y1Decimal        (g3_Y1Dec),
-        .x2Sign           (g3_X2Sign),
-        .x2Integer        (g3_X2Int),
-        .x2Decimal        (g3_X2Dec),
-        .y2Sign           (g3_Y2Sign),
-        .y2Integer        (g3_Y2Int),
-        .y2Decimal        (g3_Y2Dec),
-        .isCalculated     (g3_isCalculated)
-    );
+    // Correct state declaration (example uses 3 bits)
+    reg [2:0] state = 6;
+    reg prevStartCalculate = 0;
+
+
+    always @ (posedge clk100Khz) begin
+        prevStartCalculate <= startCalculate;
+
+        if (startCalculate && !prevStartCalculate) begin
+            state <= 0;
+            g1_isCalculated <= 0;
+            g2_isCalculated <= 0;
+            g3_isCalculated <= 0;
+            startIntersectionQuadratic <= 0;
+        end else begin
+            case (state)
+                0: begin
+                    // Set up the first intersection calculation
+                    temp_a1Sign <= a1Sign;
+                    temp_a1Integer <= a1Integer;
+                    temp_b1Sign <= b1Sign;
+                    temp_b1Integer <= b1Integer;
+                    temp_c1Sign <= c1Sign;
+                    temp_c1Integer <= c1Integer;
+
+                    temp_a2Sign <= 0;
+                    temp_a2Integer <= 0;
+                    temp_b2Sign <= 0;
+                    temp_b2Integer <= 0;
+                    temp_c2Sign <= 0;
+                    temp_c2Integer <= 0;
+
+                    startIntersectionQuadratic <= 1; // Start the intersection calculation
+                    state <= 1;
+                end
+
+                1: if (temp_isCalculated == CALCULATED || temp_isCalculated == NO_SOLUTION) begin
+                    // Store the results for Graph 1 and Graph 2
+                    g1_X1Sign <= temp_X1Sign;
+                    g1_X1Int <= temp_X1Int;
+                    g1_X1Dec <= temp_X1Dec;
+
+                    g1_Y1Sign <= 0;
+                    g1_Y1Int <= 0;
+                    g1_Y1Dec <= 0;
+
+                    g1_X2Sign <= temp_X2Sign;
+                    g1_X2Int <= temp_X2Int;
+                    g1_X2Dec <= temp_X2Dec;
+
+                    g1_Y2Sign <= 0;
+                    g1_Y2Int <= 0;
+                    g1_Y2Dec <= 0;
+
+                    g1_isCalculated <= temp_isCalculated; // Indicate that the calculation is done
+                    startIntersectionQuadratic <= 0; 
+                    state <= 2;
+                end
+
+                2: begin
+                    // Set up the second intersection calculation
+                    temp_a1Sign <= a2Sign;
+                    temp_a1Integer <= a2Integer;
+                    temp_b1Sign <= b2Sign;
+                    temp_b1Integer <= b2Integer;
+                    temp_c1Sign <= c2Sign;
+                    temp_c1Integer <= c2Integer;
+
+                    temp_a2Sign <= 0;
+                    temp_a2Integer <= 0;
+                    temp_b2Sign <= 0;
+                    temp_b2Integer <= 0;
+                    temp_c2Sign <= 0;
+                    temp_c2Integer <= 0;
+
+                    startIntersectionQuadratic <= 1; // Start the intersection calculation
+                    state <= 3;
+                end
+
+                3: if(temp_isCalculated == CALCULATED || temp_isCalculated == NO_SOLUTION) begin
+                    // Store the results for Graph 1 and Graph 3
+                    g2_X1Sign <= temp_X1Sign;
+                    g2_X1Int <= temp_X1Int;
+                    g2_X1Dec <= temp_X1Dec;
+
+                    g2_Y1Sign <= 0;
+                    g2_Y1Int <= 0;
+                    g2_Y1Dec <= 0;
+
+                    g2_X2Sign <= temp_X2Sign;
+                    g2_X2Int <= temp_X2Int;
+                    g2_X2Dec <= temp_X2Dec;
+
+                    g2_Y2Sign <= 0;
+                    g2_Y2Int <= 0;
+                    g2_Y2Dec <= 0;
+
+                    g2_isCalculated <= temp_isCalculated; // Indicate that the calculation is done
+                    startIntersectionQuadratic <= 0; 
+                    state <= 4;
+                end
+
+                4: begin
+                    // Set up the third intersection calculation
+                    temp_a1Sign <= a3Sign;
+                    temp_a1Integer <= a3Integer;
+                    temp_b1Sign <= b3Sign;
+                    temp_b1Integer <= b3Integer;
+                    temp_c1Sign <= c3Sign;
+                    temp_c1Integer <= c3Integer;
+
+                    temp_a2Sign <= 0;
+                    temp_a2Integer <= 0;
+                    temp_b2Sign <= 0;
+                    temp_b2Integer <= 0;
+                    temp_c2Sign <= 0;
+                    temp_c2Integer <= 0;
+
+                    startIntersectionQuadratic <= 1; // Start the intersection calculation
+                    state <= 5;
+                end
+
+                5: if(temp_isCalculated == CALCULATED || temp_isCalculated == NO_SOLUTION) begin
+                    // Store the results for Graph 2 and Graph 3
+                    g3_X1Sign <= temp_X1Sign;
+                    g3_X1Int <= temp_X1Int;
+                    g3_X1Dec <= temp_X1Dec;
+
+                    g3_Y1Sign <= 0;
+                    g3_Y1Int <= 0;
+                    g3_Y1Dec <= 0;
+
+                    g3_X2Sign <= temp_X2Sign;
+                    g3_X2Int <= temp_X2Int;
+                    g3_X2Dec <= temp_X2Dec;
+
+                    g3_Y2Sign <= 0;
+                    g3_Y2Int <= 0;
+                    g3_Y2Dec <= 0;
+
+                    g3_isCalculated <= temp_isCalculated; // Indicate that the calculation is done
+                    startIntersectionQuadratic <= 0; 
+                    state <= 6;
+                end
+
+                6: begin
+                    // Do nothing, final state
+                    state <= 6;
+                end
+
+                default: state <= 6;
+            endcase
+        end
+    end
 
 endmodule
+
